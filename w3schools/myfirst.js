9a5952ee9cbd9d56ea73ada4133c18e29cde30cf
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-27 17:51:37 +0000

    Created a server that tells time

---
+++ w3schools/myfirst.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+const dt = require('./myfirstmodule');
+
+http.createServer(function(req, res){
+  res.writeHead(200, {'Content-type': 'text/html'});
+  res.write("The date and time are currently: " + dt.myDateTime());
+  res.end('Hello World!');
+}).listen(8080);
