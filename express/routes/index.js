2b995ec98f2bab751ad123a7aa6522820c41877d
Mark Fitzpatrick <markfitz815@gmail.com>
2017-11-30 19:16:14 +0000

    Changed json link to only be in index.js file and turned all list items into links through json

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var vd = require('../videodata.json');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
     title: 'Express',
-    myName: 'Mark F'
+    myName: 'Mark F',
+    videodata: vd
   });
 });
 
