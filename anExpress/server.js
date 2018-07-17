5ae91ff063e8c411995bdce6ee8b71668a290d7d
Mark Fitzpatrick <markfitz815@gmail.com>
2018-07-17 21:52:37 +0100

    able to send html file over express

--- anExpress/server.js
+++ anExpress/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 
 //Main route for index
 app.get('/', (req, res) => {
-    res.send('Hello User!');
+    res.sendFile(path.join(__dirname + '/views/index.html'));
+})
+
+app.get('/test', (req, res) => {
+    res.send('This is test');
 })
 
 //Tell app to listen on port 3000
