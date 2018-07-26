7ffcf0ef8dece3c160829d3c1df2fc0c7763f746
Mark Fitzpatrick <markfitz815@gmail.com>
2018-07-26 20:31:18 +0100

    added styling and area for entering name

---
+++ chatApp/public/scripts/indexSocket.js
@@ -0,0 +1,48 @@
+var socket = io.connect('http://localhost:3000');
+
+let chatData = [];
+let userData = {
+    name: ''
+}
+
+function writeChat(data){
+    let currMsg = chatData[chatData.length -1].msg;
+    const newEl = document.createElement('div');
+    newEl.classList.add('chatMsg');
+    const newElMsg = document.createElement('div');
+    newElMsg.innerHTML = currMsg;
+    newEl.appendChild(newElMsg);
+    document.querySelector('#chatMain').appendChild(newEl);
+}
+
+function sendMsg(){
+    const userMsg = document.querySelector('#msgInput').value;
+    socket.emit('message', {msg: userMsg});
+    document.querySelector('#msgInput').value = '';
+}
+
+socket.on('news', function (data) {
+console.log(data);
+socket.emit('my other event', { my: 'data' });
+});
+
+socket.on('message', (data) => {
+    chatData.push(data);
+    //document.querySelector('#chatMain').innerHTML += '<br>' + data.msg;
+    writeChat(chatData);
+})
+
+document.querySelector('#msgSend').addEventListener('click', () => {
+    sendMsg();
+})
+document.querySelector('#msgInput').addEventListener('keydown', (e) => {
+    if(e.keyCode == 13) { 
+        sendMsg();
+        e.preventDefault();
+    }
+});
+
+
+document.querySelector('#chatMain').addEventListener('scroll', (evt) => {
+    console.log(evt);
+})
