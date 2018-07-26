94c47aee269df81689a342379ec1d251e8152b9b
Mark Fitzpatrick <markfitz815@gmail.com>
2018-07-26 13:44:11 +0100

    added css to html and socket responses are sent to chat div

--- chatApp/app.js
+++ chatApp/app.js
@@ -1,14 +1,17 @@
+var express = require('express');
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 const port = 3000;
+
+app.use(express.static(__dirname + '/public'));
 
 server.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 io.on('connection', function (socket) {
@@ -18,5 +21,6 @@ io.on('connection', function (socket) {
   });
   socket.on('message', (data) => {
       console.log(data);
+      socket.emit('message', data);
   })
 });
