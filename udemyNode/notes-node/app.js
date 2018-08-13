08a331cef7b8dcd1a32f829319a67ecf4195b8e5
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-13 20:09:17 +0100

    udemy tutorial: checking argv with if else statements

--- udemyNode/notes-node/app.js
+++ udemyNode/notes-node/app.js
@@ -1,22 +1,22 @@
 console.log('Starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
 
-// var res = notes.addNote();
-// console.log(res);
+var command = process.argv[2];
 
-//Removes duplicates from array
-console.log(_.uniq(['Mark',2,5,2,5,10,2,5,10,20,'Mark', 'mark']))
+console.log('Command: ', command);
 
-//Checks if element is string
-// console.log(_.isString(true));
-// console.log(_.isString('Mark'));
-
-// console.log('Result:', notes.add(10, 55));
-// var user = os.userInfo();
-// fs.appendFile('greetings.txt', `Hello ${user.username} and you are ${notes.age}!`, (err) => {
-//     if(err) {console.log('An error has occured')}
-// });
+if(command === 'add') {
+    console.log('adding new note');
+} else if (command === 'list') {
+    console.log('listing all notes');
+} else if (command === 'read') {
+    console.log('fetching note')
+} else if (command === 'remove') {
+    console.log('removing note');
+} else {
+    console.log('command not recognized');
+}
