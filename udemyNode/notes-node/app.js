444c731414f7aba745819d0f8d917220e738a5b9
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-13 19:21:35 +0100

    udemy tutorial: working with lodash to identify strings and filter arrays

--- udemyNode/notes-node/app.js
+++ udemyNode/notes-node/app.js
@@ -2,14 +2,20 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
 // var res = notes.addNote();
 // console.log(res);
 
+//Removes duplicates from array
+console.log(_.uniq(['Mark',2,5,2,5,10,2,5,10,20,'Mark', 'mark']))
 
-console.log('Result:', notes.add(10, 55));
+//Checks if element is string
+// console.log(_.isString(true));
+// console.log(_.isString('Mark'));
 
+// console.log('Result:', notes.add(10, 55));
 // var user = os.userInfo();
 // fs.appendFile('greetings.txt', `Hello ${user.username} and you are ${notes.age}!`, (err) => {
 //     if(err) {console.log('An error has occured')}
