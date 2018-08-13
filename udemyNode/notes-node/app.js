5c706e893a84c0f6ba43ae063f78a822deba68a5
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-13 22:06:59 +0100

    udemy tutorial: working with yargs to pass aguments into app from command line and check them in if else statment

--- udemyNode/notes-node/app.js
+++ udemyNode/notes-node/app.js
@@ -2,21 +2,24 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-var command = process.argv[2];
+const argv = yargs.argv;
+var command = argv._[0];
 
 console.log('Command: ', command);
+console.log('Yargs', argv);
 
 if(command === 'add') {
-    console.log('adding new note');
+    notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-    console.log('listing all notes');
+    notes.getAll();
 } else if (command === 'read') {
-    console.log('fetching note')
+    notes.getNote(argv.title);
 } else if (command === 'remove') {
-    console.log('removing note');
+    notes.removeNote(argv.title);
 } else {
     console.log('command not recognized');
 }
