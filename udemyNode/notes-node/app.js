9178b475ca0d517a8a2b65a2d48a3b2b10601964
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-15 14:35:02 +0100

    udemy tutorial: getList function now retrives entire list

--- udemyNode/notes-node/app.js
+++ udemyNode/notes-node/app.js
@@ -1,5 +1,3 @@
-console.log('Starting app.js');
-
 const fs = require('fs');
 const _ = require('lodash');
 const yargs = require('yargs');
@@ -9,9 +7,6 @@ const notes = require('./notes.js');
 const argv = yargs.argv;
 var command = argv._[0];
 
-console.log('Command: ', command);
-console.log('Yargs', argv);
-
 if(command === 'add') {
     let note = notes.addNote(argv.title, argv.body);
     if(note) {
@@ -20,18 +15,20 @@ if(command === 'add') {
     } else {
         console.log('Duplicate note entered');
     }
-} else if (command === 'list') {
-    notes.getAll();
-} else if (command === 'read') {
-    const readNote = notes.getNote(argv.title);
-    if(readNote) {
-        notes.logNote(readNote);
-    } else{ console.log(`Message not found with title: '${argv.title}'`) }
-} else if (command === 'remove') {
-    const noteRemoved = notes.removeNote(argv.title);
-    let message = noteRemoved ? 'Note was removed' : 'Note not found';
-    
-} else {
-    console.log('command not recognized');
+    } else if (command === 'list') {
+        const allNotes = notes.getAll();
+        console.log(`Printing ${allNotes.length} note(s).`);
+        allNotes.forEach(el => notes.logNote(el));
+    } else if (command === 'read') {
+        const readNote = notes.getNote(argv.title);
+        if(readNote) {
+            notes.logNote(readNote);
+        } else{ console.log(`Message not found with title: '${argv.title}'`) }
+    } else if (command === 'remove') {
+        const noteRemoved = notes.removeNote(argv.title);
+        let message = noteRemoved ? 'Note was removed' : 'Note not found';
+        
+    } else {
+        console.log('command not recognized');
 }
 
---
+++ udemyNode/notes-node/playground/debugging.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Mark'
+};
+
+person.age = 27;
+debugger;
+person.name = 'Mark Anthony'
+
+console.log(person);
