463584a3d9f5a0ff7b34496c490d3d68f423bc90
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-16 14:02:42 +0100

    udemy tutorial: error handling added app

--- udemyNode/weather-app/app.js
+++ udemyNode/weather-app/app.js
@@ -14,18 +14,24 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-console.log(argv);
 
-const apiKey = ''
+
+const apiKey = '';
 const inpAddress = encodeURIComponent(argv.a);
-
-console.log(argv);
 
 request({
     url: `https://maps.google.com/maps/api/geocode/json?address=${inpAddress}&key=${apiKey}`,
     json: true
 }, (error ,response, body) => {
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    if (error) {
+        console.log('Unable to connect to Google servers.');
+    } else if (body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find that address');
+    } else if (body.status === 'OK') {
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+    } else {
+        console.log('An error has occured');
+    }
 });
