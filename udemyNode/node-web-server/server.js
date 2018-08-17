97dec275ab64af747e16e009c5ef2f90eb858f44
Mark Fitzpatrick <markfitz815@gmail.com>
2018-08-17 11:20:26 +0100

    udemy tutorial: using express middleware to make server log report

--- udemyNode/node-web-server/server.js
+++ udemyNode/node-web-server/server.js
@@ -1,11 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const log = `${now}: ${req.method} ${req.url}`
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
 app.use(express.static(__dirname + '/public'));
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
